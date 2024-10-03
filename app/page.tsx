61d286b42c1d814fbc44d411c2f7693654c79c48
Dieumerci MUHINDO <dieumerci.md09@gmail.com>
2024-10-03 22:46:24 +0200

    feat : add shadcn ui on the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import { Input } from "@/components/ui/input"
 
 export default function Home() {
   return (
-    <main>
-      Salut
+    <main className="w-full">
+      <div className="w-72 p-2 flex h-screen flex-col gap-6 justify-center items-center m-auto">
+        <span>Authentification</span>
+        <Input type="email" placeholder="Email" className="w-full" />
+      </div>
     </main>
   );
 }
