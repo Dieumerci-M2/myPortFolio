55e8b1dff524a188126ac5fc2943c594670a8e51
Dieumerci MUHINDO <dieumerci.md09@gmail.com>
2024-10-10 17:15:34 +0200

    feat : add Animation for screen transitions

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import {JetBrains_Mono} from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const JetBrainMono = JetBrains_Mono({
   subsets : ["latin"],
@@ -31,6 +32,8 @@ export default function RootLayout({
         className={`${JetBrainMono.variable}  antialiased`}
       >
         <Header />
+
+        <StairTransition />
         <PageTransition>
 
           {children}
