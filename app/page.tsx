93e4a7c6eea493459d0f2f66858a32a95cca85e7
Dieumerci MUHINDO <dieumerci.md09@gmail.com>
2024-10-18 15:52:56 +0200

    feat : add my image and a rotate circle arround it

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <main className="h-full">
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left">
+          <div className="order-2 xl:order-none text-center xl:text-left">
             <span className="text-xl"> Software Developer</span>
             <h1 className="H1 mb-6">
               Hello I'm <br />
@@ -34,7 +34,7 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div>
+          <div className="order-1 xl:order-none xl:mb-0">
             <Photo />
           </div>
         </div>
