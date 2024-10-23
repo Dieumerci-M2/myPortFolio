7740545b0771915ecafa0cf184b97e52394c4ed0
Dieumerci MUHINDO <dieumerci.md09@gmail.com>
2024-10-23 20:14:41 +0200

    feat : finish with Home page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,8 +1,46 @@
-import React from 'react'
+"use client";
+
+import {BsArrowDownRight} from "react-icons/bs"
+import Link from "next/link"
+import {motion} from "framer-motion"
+
+const Allservices = [
+  {
+    num : "01",
+    title : "Web Development",
+    description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis repellat ut illo, repellendus dolores aspernatur optio ipsum odio magni."
+  },
+  {
+    num : "02",
+    title : "UI/UX Design",
+    description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis repellat ut illo, repellendus dolores aspernatur optio ipsum odio magni."
+  },
+  {
+    num : "03",
+    title : "Web Development",
+    description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis repellat ut illo, repellendus dolores aspernatur optio ipsum odio magni."
+  },
+  {
+    num : "04",
+    title : "Web Development",
+    description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis repellat ut illo, repellendus dolores aspernatur optio ipsum odio magni."
+  },
+]
 
 const services = () => {
   return (
-    <div>services</div>
+    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-8"> 
+      <div className="container mx-auto">
+        
+        {Allservices.map((service, index)=>{
+          return(
+            <div className="" key={index}>
+                {service.title} 
+            </div>
+          )
+        })}
+      </div>
+    </section>
   )
 }
 
